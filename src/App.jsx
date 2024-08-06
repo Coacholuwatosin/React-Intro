3cdbf2ca868cd2b2ace822908dfb7f4ea35b40d9
oluwatosin adegboye <adegboyeoluwatosin2@gmail.com>
2024-08-06 10:44:33 +0100

    first intro

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis facilis sapiente ullam dolorum itaque cupiditate quam alias repudiandae, minima sequi!
+        </p>
+    </>
+  )
+}
+
+export default Footer
